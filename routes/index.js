f54d91f0521ec94518f3d9da769c9a151c4b7158
Cory Ferrier <ferriercory@gmail.com>
2024-07-13 16:45:25 -0400

    Move around code in index.js so key declarations are first, than class definitions, than function definitions, then routes, then startup code

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ let express = require('express');
 let fs = require('fs');
 let router = express.Router();
 
-let current_song = "The Piper";
-let fetched_song, song_entry, album_name, album, song, cover_art_url;
+let songs = new Map();
+
 let headers = new Headers({
     "Accept"       : "application/json",
     "Content-Type" : "application/json",
@@ -26,28 +26,6 @@ function Album(id, note, is_loved = false) {
 	this.id = id;
 	this.note = note;
 	this.is_loved = is_loved;
-}
-
-/* Intialize our list of songs, with artist and album mappings.
-   The notes for each album/artist/song should be structured in 
-   a way they can flow into eachother like sentences.
-   The ids are musicbrainz api ids.
-*/
-let songs = new Map();
-let data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-let artists = data.artists;
-// Not sure why for...in isn't working here. Do this instead.
-for (let i = 0; i < artists.length; i++) {
-	let artist = artists[i];
-	let ar = new Artist(artist.name, artist.note);
-	for (let j = 0; j < artist["albums"].length; j++) {
-		let album = artist.albums[j];
-		let al = new Album(album.id, album.note, album.is_loved);
-		for (let k = 0; k < album["songs"].length; k++) {
-			let song = album.songs[k];
-			songs.set(song.name, new Song(song.id, ar, al, song.note));
-		}
-	}
 }
 
 // Gets info on an album from the musicbrainz API.
@@ -143,6 +121,31 @@ router.post('/current_song', function(req, res, next) {
   res.send("set song to " + current_song);
 });
 
-// Update Song Data on Start.
+/* Startup Code Below */
+
+/* Intialize our list of songs, with artist and album mappings.
+   The notes for each album/artist/song should be structured in 
+   a way they can flow into eachother like sentences.
+   The ids are musicbrainz api ids.
+*/
+let data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+let artists = data.artists;
+// Not sure why for...in isn't working here. Do this instead.
+for (let i = 0; i < artists.length; i++) {
+	let artist = artists[i];
+	let ar = new Artist(artist.name, artist.note);
+	for (let j = 0; j < artist["albums"].length; j++) {
+		let album = artist.albums[j];
+		let al = new Album(album.id, album.note, album.is_loved);
+		for (let k = 0; k < album["songs"].length; k++) {
+			let song = album.songs[k];
+			songs.set(song.name, new Song(song.id, ar, al, song.note));
+		}
+	}
+}
+
+// Pick an arbitrary song and update Song Data on Start.
+let current_song = "The Piper";
+let fetched_song, song_entry, album_name, album, song, cover_art_url;
 UpdateSongData();
 module.exports = router;
