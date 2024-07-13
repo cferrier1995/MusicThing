d5a620c7256d39de5747485daf7a391f3591d95e
Cory Ferrier <ferriercory@gmail.com>
2024-07-13 16:13:57 -0400

    Make UpdateSong (lazily) update the song instead of having this done by the getter method. Also do this on launch. This will reduce API calls at the actual event and make things load a lot faster.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ let fs = require('fs');
 let router = express.Router();
 
 let current_song = "The Piper";
-
+let fetched_song, song_entry, album_name, album, song, cover_art_url;
 let headers = new Headers({
     "Accept"       : "application/json",
     "Content-Type" : "application/json",
@@ -43,10 +43,8 @@ for (let i = 0; i < artists.length; i++) {
 	for (let j = 0; j < artist["albums"].length; j++) {
 		let album = artist.albums[j];
 		let al = new Album(album.id, album.note, album.is_loved);
-		console.log(album["songs"].length);
 		for (let k = 0; k < album["songs"].length; k++) {
 			let song = album.songs[k];
-			console.log(song);
 			songs.set(song.name, new Song(song.id, ar, al, song.note));
 		}
 	}
@@ -55,7 +53,6 @@ for (let i = 0; i < artists.length; i++) {
 // Gets info on an album from the musicbrainz API.
 async function GetAlbum(release_id) {
   const url = "http://musicbrainz.org/ws/2/release/" + release_id;
-  console.log(url);
   try {
     const response = await fetch(url, {
     method  : 'GET', 
@@ -75,7 +72,6 @@ async function GetAlbum(release_id) {
 // Gets the cover art from the cover art archive.
 async function GetCoverArt(release_id) {
   const url = "http://coverartarchive.org/release/" + release_id;
-  console.log(url);
   try {
     const response = await fetch(url, {
     method  : 'GET', 
@@ -95,7 +91,6 @@ async function GetCoverArt(release_id) {
 // Gets info on a song from the musicbrainz API.
 async function GetSong(song_id) {
   const url = "http://musicbrainz.org/ws/2/recording/" + song_id;
-  console.log(url);
   try {
     const response = await fetch(url, {
     method  : 'GET', 
@@ -112,43 +107,42 @@ async function GetSong(song_id) {
   }
 }
 
+async function UpdateSongData() {
+	song_entry = songs.get(current_song);
+	album = await GetAlbum(song_entry.album.id);
+	song = await GetSong(song_entry.id);
+	if (album["cover-art-archive"].front) {
+	let cover_art = await GetCoverArt(song_entry.album.id);
+		for (let i = 0; i < cover_art.images.length; i++) {
+			let image = cover_art.images[i];
+			if (image.front == true) {
+				cover_art_url = image.image;
+			}
+		}
+	}
+	album_name = album.title;
+	if (album_name == song_entry.artist.name) {
+		album_name = "Self-Titled";
+	}
+	else if (album_name == song.title) {
+		album_name = "Single";
+	}
+	fetched_song = current_song;
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-(async() => {
-  // Get data from the musicbrainz api for the correct names and such of the artist/album/ect.
-  // Fetch cover art if the album response indicates we have it.
-  // Doing this with async calls isn't great but I plan on caching this anyways.
-  console.log(current_song);
-  let song_entry = songs.get(current_song);
-  let album = await GetAlbum(song_entry.album.id);
-  let song = await GetSong(song_entry.id);
-  let cover_art_url;
-  if (album["cover-art-archive"].front) {
-	  let cover_art = await GetCoverArt(song_entry.album.id);
-	  for (let i = 0; i < cover_art.images.length; i++) {
-		let image = cover_art.images[i];
-	    if (image.front == true) {
-			cover_art_url = image.image;
-		}
-	  }
-  }
-  let album_name = album.title;
-  if (album_name == song_entry.artist.name) {
-	  album_name = "Self-Titled";
-  }
-  else if (album_name == song.title) {
-	  album_name = "Single";
-  }
   res.render('index', {artist: song_entry.artist.name, year: album.date.substr(0,4), artist_note: song_entry.artist.note, album: album_name, album_note: song_entry.album.note, song: song.title, song_note: song_entry.note, cover_art_url:cover_art_url});
-})()
 });
 
 /* POST Route to set the current song, called by polling_script.js in the browser window. */
 router.post('/current_song', function(req, res, next) {
-  console.log(req.body);
   current_song = req.body.song_name;
+  // A new song has been pushed, refresh our data.
+  UpdateSongData();
   res.send("set song to " + current_song);
 });
 
+// Update Song Data on Start.
+UpdateSongData();
 module.exports = router;
